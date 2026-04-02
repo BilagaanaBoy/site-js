5a0dd583627ba87c3aa3580740d3f83f97087474
BilagaanaBoy <losharals@yandex.ru>
2026-04-03 02:28:38 +0300

    123123

--- js/products.js
+++ js/products.js
@@ -49,7 +49,7 @@ const products = [
     },
 
     {
-        id: "ITEMCODE6",
+        id: "ITEMCODE7",
         series: "Charger",
         model: "Блок питания",
         prices: "2000",
@@ -57,7 +57,7 @@ const products = [
         like: ''
     },
     {
-        id: "ITEMCODE6",
+        id: "ITEMCODE8",
         series: "Charger",
         model: "MagSafe провод (1 m)",
         prices: "2500",
@@ -65,7 +65,7 @@ const products = [
         like: 'hidden'
     },
     {
-        id: "ITEMCODE6",
+        id: "ITEMCODE9",
         series: "Charger",
         model: "Провод (usb-c на usb-c)",
         prices: "1500",
@@ -73,11 +73,60 @@ const products = [
         like: ''
     },
     {
-        id: "ITEMCODE6",
+        id: "ITEMCODE10",
         series: "Charger",
         model: "Пауэрбанк ",
         prices: "4000",
         photo: "items/charger-4-view.jpg",
+        like: 'hidden'
+    },
+    {
+        id: "ITEMCODE11",
+        series: "Charger",
+        model: "Докстанция для устройств",
+        prices: "7000",
+        photo: "items/charger-5-view.jpg",
+        like: 'hidden'
+    },
+    {
+        id: "ITEMCODE12",
+        series: "Charger",
+        model: "Докстанция для 2ух устройств",
+        prices: "9000",
+        photo: "items/charger-6-view.jpg",
+        like: 'hidden'
+    },
+
+    {
+        id: "ITEMCODE13",
+        series: "Charger",
+        model: "Переходник для наушников",
+        prices: "1000",
+        photo: "items/accessories-1-view.jpg",
+        like: ''
+    },
+    {
+        id: "ITEMCODE14",
+        series: "Charger",
+        model: "Ремешок для телефона",
+        prices: "4000",
+        photo: "items/accessories-2-view.jpg",
+        like: 'hidden'
+    },
+    {
+        id: "ITEMCODE15",
+        series: "Charger",
+        model: "Картхолдер на телефон",
+        prices: "3000",
+        photo: "items/accessories-3-view.jpg",
+        like: 'hidden'
+    },
+    {
+        id: "ITEMCODE16",
+        series: "Charger",
+        model: "Переходник для телефона",
+        prices: "2000",
+        photo: "items/accessories-4-view.jpg",
         like: 'hidden'
     },
 
