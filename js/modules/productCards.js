35da32ceced8897ef61430a1f93bd0e07ec2add2
BilagaanaBoy <losharals@yandex.ru>
2026-04-03 01:37:01 +0300

    123

--- js/modules/productCards.js
+++ js/modules/productCards.js
@@ -24,6 +24,31 @@ const renderProductCard = ({id, photo, model, prices, like, series}, value) => {
         `;
 
         return li;
+    } else if (value == 'like') {
+        const li = document.createElement('li');
+        li.classList.add('product', 'item', 'column', 'aic', 'js-product');
+        li.innerHTML =  `
+            <div class="favorites js-favorites">
+                <span class="heart-filled js-heart-filled ${like}">
+                    <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fill-rule="nonzero"></path></svg> 
+                </span>
+            </div>
+            <a href="#" id="${id}" class="link column aic js-link-card">
+                
+                <div class="product-image row jcc">
+                    <img src="img/products/${photo}" alt="" class="image js-image-card">   
+                </div>
+                <div class="product-description">
+                    <h3 class="title js-title-card">${model}</h3>
+                </div>  
+                <div class="product-price">
+                    <span class="price js-price-card">${prices}</span><span>₽</span>
+                </div>       
+            </a>
+            <button type="button" class="addCart buy-button js-buy-button">В корзину</button>
+        `;
+
+        return li;
     } else if (series == value) {
         const li = document.createElement('li');
         li.classList.add('product', 'item', 'column', 'aic', 'js-product');
@@ -88,11 +113,10 @@ const renderProductCards = (products, container, value) => {
     products.forEach((e) => {
         const card = renderProductCard(e, value);
         
-        console.log(card)
-        // if (condition) {
-            
-        // }
-        appendProductCard(card, container);
+        if (typeof(card) != "undefined") {
+            appendProductCard(card, container);
+        }
+        
     });
 };
 
