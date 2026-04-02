6194ad84a5084396448e77b108abc3e52d51ae55
BilagaanaBoy <losharals@yandex.ru>
2026-04-02 03:47:49 +0300

    1

--- js/modules/cartPopup.js
+++ js/modules/cartPopup.js
@@ -3,6 +3,21 @@ const overlay = document.querySelector('.js-overlay');
 const openCartButton = document.querySelector('.js-cart-btn');
 const closeCartElements = document.querySelectorAll('.js-close-cart');
 
+const getScrollBarWidth = () => {
+    let div = document.createElement('div');
+    div.style.width = '100px';
+    div.style.height = '100px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    document.body.append(div);
+    let scrollBarWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollBarWidth;
+}
+
+const scroll = getScrollBarWidth();
+
 const openCart = () => { 
     openCartButton.addEventListener('click', () => {
         document.body.style.overflow = 'hidden';
