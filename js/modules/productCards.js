c2afd134ee195189120ddbc5ee3d68b45b75d558
BilagaanaBoy <losharals@yandex.ru>
2026-04-03 00:46:34 +0300

    123

--- js/modules/productCards.js
+++ js/modules/productCards.js
@@ -4,18 +4,9 @@ const renderProductCard = ({id, photo, model, prices, like}) => {
     li.classList.add('product', 'item', 'column', 'aic', 'js-product');
     li.innerHTML =  `
         <div class="favorites js-favorites">
-            ` 
-                (like) ? 
-                `<span class="heart-filled js-heart-filled">
+            <span class="heart-filled js-heart-filled ${like}">
                 <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fill-rule="nonzero"></path></svg> 
-                </span>`
-                            
-                :
-                
-                `<span class="heart-filled js-heart-filled hidden">
-                <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fill-rule="nonzero"></path></svg> 
-                </span>`
-            `
+            </span>
         </div>
         <a href="#" id="${id}" class="link column aic js-link-card">
             
