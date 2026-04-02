7b46f0cb6bb98d9c7d2fdcd0e4e8fcfdd9db7abe
BilagaanaBoy <losharals@yandex.ru>
2026-04-02 03:42:45 +0300

    111

--- js/modules/cartPopup.js
+++ js/modules/cartPopup.js
@@ -5,6 +5,8 @@ const closeCartElements = document.querySelectorAll('.js-close-cart');
 
 const openCart = () => { 
     openCartButton.addEventListener('click', () => {
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
         cart.classList.add('active');
         overlay.classList.add('active');
     })
@@ -13,6 +15,8 @@ const openCart = () => {
 const closeCart = () => { 
     closeCartElements.forEach((e) => {
         e.addEventListener('click', () => {
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
             cart.classList.remove('active');
             overlay.classList.remove('active');
         });
