0c56b9cbcada304cb91b8f5a68fb3f64e3f58ba7
BilagaanaBoy <losharals@yandex.ru>
2026-04-02 04:13:37 +0300

    12

--- js/modules/cartData.js
+++ js/modules/cartData.js
@@ -3,8 +3,27 @@ const cartData = () => {
 
     const updateCartItemCount = () => { 
         cart.addEventListener('click', (e) => {
+            
+            let currentItems, minusBt;
             if(e.target.matches('.js-minus') || e.target.matches('.js-plus')){
-                console.log(1)
+                const counter = e.target.closest('js-counter');
+                currentItems = e.target.querySelector('js-current-items');
+            }
+
+            if (e.target.matches('.js-plus')) {
+                currentItems.textContent = ++currentItem.textContent;
+                minusBt.classList.remove('disabled');
+            }
+
+            if (e.target.matches('.js-minus')) {
+
+                if (parseInt(currentItems.textContent) > 2) {
+                    currentItems.textContent = --currentItem.textContent;
+                } else if (parseInt(currentItems.textContent) === 2) {
+                    currentItems.textContent = --currentItem.textContent;
+                    minusBt.classList.add('disabled');
+                }
+                
             }
         })
     };
