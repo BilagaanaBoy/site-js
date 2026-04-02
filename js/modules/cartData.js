e15007a6ec4695badbec4e7201ccc95178aebb40
BilagaanaBoy <losharals@yandex.ru>
2026-04-02 04:46:53 +0300

    12123

--- js/modules/cartData.js
+++ js/modules/cartData.js
@@ -1,5 +1,9 @@
 const cartData = () => { 
     const cart = document.querySelector('.js-cart');
+    const productsList = document.querySelector('.js-products-list');
+    const cartList = document.querySelector('.js-cart-list');
+
+    const productInfo = {};
 
     const updateCartItemCount = () => { 
         cart.addEventListener('click', (e) => {
@@ -26,8 +30,71 @@ const cartData = () => {
             }
         })
     };
-
     updateCartItemCount();
+
+    const addProductToCart = () => {
+        productsList.addEventListener('click', (e) => {
+            if(e.target.classList.contains('js-buy-button')) {
+                const product = e.target.closest('.js-product');
+
+                const imageCard = product.querySelector('.js-image-card');
+                const modelCard = product.querySelector('.js-title-card');
+                const priceCard = product.querySelector('.js-price-card');
+                const linkCard = product.querySelector('.js-link-card');
+
+                productInfo.id = linkCard.getAttribute('id');
+                productInfo.model = modelCard.textContent;
+                productInfo.price = priceCard.textContent;
+                productInfo.photo = imageCard.src;
+                
+                const productInCart = cartList.querySelector(`#${productInfo.id}`)
+                if (productInCart) {
+                    const currentItemsProduct = productInCart.querySelector('.js-current-items');
+                    currentItemsProduct.textContent = parseInt(currentItemsProduct.textContent) + 1;\
+
+                    const minusBt = productInCart.querySelector('.js-minus');
+                    minusBt.classList.remove('disabled');
+                } else {
+                    renderProductCard();
+                }
+                
+            }
+        })
+    }
+    addProductToCart();
+
+    const renderProductCard = () => { 
+        const li = document.createElement('li');
+        li.classList.add('cart-item', 'column', 'js-cart-item');
+
+        li.innerHTML =  `
+            <span class="close"></span>
+            <div class="cartline row jcfs aic" id="${productInfo.id}">
+                <div class="cart-image-container">
+                    <img src="${productInfo.photo}" alt="" class="cart-img">
+                </div>
+                <div class="column">
+                    <div class="cart-model row jcfs aic">
+                        ${productInfo.model}
+                    </div>
+                    <div class="row jcsb aic">
+                        <div class="counter row jcc aic js-counter">
+                            <button type="button" class="minus control row jcc aic js-minus disabled" >-</button>
+                            <div class="current-items row jcc aic js-current-items">1</div>
+                            <button type="button" class="plus control row jcc aic js-plus">+</button>
+                        </div>
+                        <div class="row jcc aic">
+                            <span class="cart-price  row jcfe" data-price="${productInfo.price}">${productInfo.price}</span>
+                            <span class="rouble">₽</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        cartList.append(li);
+    }
+    
 }
 
 
